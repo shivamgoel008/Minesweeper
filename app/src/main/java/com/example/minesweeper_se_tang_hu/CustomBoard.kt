0a4da5dc0e48693ec865f80643aa0a41a572c581
Shivam Goel <55030452+shivamgoel008@users.noreply.github.com>
2021-04-03 22:30:06 +0530

    collapsing issue done

--- app/src/main/java/com/example/minesweeper_se_tang_hu/CustomBoard.kt
+++ app/src/main/java/com/example/minesweeper_se_tang_hu/CustomBoard.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import kotlinx.android.synthetic.main.activity_custom_board.*
 
 class CustomBoard : AppCompatActivity() {
@@ -12,18 +14,18 @@ class CustomBoard : AppCompatActivity() {
         setContentView(R.layout.activity_custom_board)
 
 
-/* edit text */
-        val height: EditText =findViewById(R.id.hi)
-        val width: EditText =findViewById(R.id.wi)
-        val mines: EditText =findViewById(R.id.mi)
+// edit text
+        val height =findViewById<TextInputLayout>(R.id.hi)
+        val width =findViewById<TextInputLayout>(R.id.wi)
+        val mines =findViewById<TextInputLayout>(R.id.mi)
 
 
 /* when user clicks submit button */
         submit.setOnClickListener {
             //after clicking submit button, this will transfer or pass the value which the user has entered
-            var heigh= Integer.parseInt(height.text.toString())
-            var widt= Integer.parseInt(width.text.toString())
-            var mine= Integer.parseInt(mines.text.toString())
+            var heigh= Integer.parseInt(height.editText?.text.toString())
+            var widt= Integer.parseInt(width.editText?.text.toString())
+            var mine= Integer.parseInt(mines.editText?.text.toString())
 
 /* passing the values to the gameplay activity */
             val intent= Intent(this, GamePlay::class.java).apply{
